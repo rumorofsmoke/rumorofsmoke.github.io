943879a91af06f38515c4b8429cc22a965bc6608
rumorofsmoke <61155074+rumorofsmoke@users.noreply.github.com>
2022-11-13 15:47:58 +0100

    Add files via upload

--- calculator_html/sine_cal.js
+++ calculator_html/sine_cal.js
@@ -3,93 +3,120 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
  */
 
-
-
-
+/* need a more elegant way of declaring const or rather define like in "C" */
 const MAX_SIZE=255; 
 const MULTI_PI_BASE=2;
 const MID_POINT=MAX_SIZE / 2;
 const TABLE_SIZE=256; 
-
+  var chart; 
 /* BOOT UP TIME ! */  
 
-//alert('not all function are implemented, at the moment only sine works !');
+alert('only sine modulator available at the moment !');
 
+// don't call things that don't exit ! 
+/*
 var compute_button = document.querySelector(".user_button");
-compute_button.addEventListener("click",calsine); 
+    compute_button.addEventListener("click",calsine); 
+*/
+var     mult_slider  = document.querySelector("#mult_freq"); 
+        mult_slider.addEventListener("input", refresh_slider_value);
+        mult_slider.addEventListener("input", calsine);
+    
+ var    modulated_sel = document.querySelector("#sel_waveform"); 
+        modulated_sel.addEventListener('change',calsine);
+        
+ /*
+  * for next use ! at this point we only use sine modulator 
+ var    modulator_sel = document.querySelector("#sel_modulator"); 
+        modulator_sel.addEventListener('change',calsine);
+*/
+    
+//for the function to be call after the page is fully loaded, we use this ! 
+document.addEventListener('DOMContentLoaded', calsine); 
+document.addEventListener('DOMContentLoaded', refresh_slider_value); 
+    
+var span_slider_mult = document.querySelector("#span_mult_freq"); 
+
+refresh_slider_value(); 
 
 
-// just to have something to show at boot up ! 
-var x = new Array();  
- 
-for (let i=0; i<MAX_SIZE; i++)
+function refresh_slider_value()
 {
-    x[i]=i; 
+   span_slider_mult.span_slider_mult.innerText = span_slider_mult.textContent =  mult_slider.value.toString(); 
 }
- build_chart(x,x); 
- 
- 
- 
- 
- 
- /* FUNCTIONS */ 
 
-function calsine () 
+function calsine() 
 {
-  var user_sine = new Array();
-  var sinewave = new Array();
-  var mult_sinewave =new Array(); 
-  
-  var user_multiplier; 
+  var user_modwave = new Array();
+  var userwave = new Array();
 
-//retrieve data for computation 
+//retrieve remaining datas for computation 
  var raw_text              = document.querySelector("#txt_raw_data");
- //var modulated_waveform     = document.querySelector("#sel_waveform"); 
- //var modulator_waveform     = document.querySelector("#sel_modulator_freq");
- var modulator_multiplier   = document.querySelector("#sel_modulator_freq");
-
-//what a mouthfull this one ...convert string to int
-user_multiplier = parseInt(modulator_multiplier.options[modulator_multiplier.selectedIndex].value);
-
-
+ var modulator_waveform    = document.querySelector("#sel_modulator_waveform");
  
-
-//console.log(user_multiplier); 
+ var y_axis = new Array(); 
  
-
+ //reset value to the block text
   raw_text.value =''; 
   
-  
+  //create the modulated waveform base ! 
      for (let i=0; i<TABLE_SIZE; i++)
      {
-         //base sine
-       sinewave[i] = Math.floor( (Math.sin(i * MULTI_PI_BASE * Math.PI / TABLE_SIZE)+1) * MID_POINT);   
-       
-       // use temp sine for the modulator sine 
-       user_sine[i] = Math.floor( (Math.sin(i * user_multiplier * Math.PI / TABLE_SIZE)+1) * MID_POINT);   
-       
-       // modulate baby ! 
-       mult_sinewave[i] = ((sinewave[i] * user_sine[i]) >> 8)+1; 
-      
-     //   console.log(mult_sinewave[i]);
-       
+            //select which waveform we build first
+            switch(modulated_sel.options[modulated_sel.selectedIndex].text)
+            {
+                case "SINE":
+                userwave[i] = Math.floor( (Math.sin(i * MULTI_PI_BASE * Math.PI / TABLE_SIZE)+1) * MID_POINT);  
+                break;
+                
+                case "TRIANGLE":
+                if(i<127)   userwave[i] = i *2; 
+                else        userwave[i] = (255 - i) * 2;
+                
+                break; 
+            
+                case "SQUARE": 
+                if(i & 0x80)   userwave[i] = 255; 
+                else           userwave[i] = 0; 
+                break; 
+            
+                case "SAW": 
+                userwave[i] = i;
+                break; 
+            
+                case "NOISE": 
+                userwave[i] = Math.floor((Math.random()*(255-0+1)));
+                break; 
+            }
+            
+        console.log(userwave[i]); 
+
+        user_modwave[i] = Math.floor( (Math.sin(i * mult_slider.value * Math.PI / TABLE_SIZE)+1) * MID_POINT);  
+          
+       // we need to make a expection if the modulation freq is set 0, in this case we only show the modulated waveform
+       if(mult_slider.value !== "0") userwave[i] = ((userwave[i] * user_modwave[i]) >> 8)+1;  
+
        // write the binary data for export 
-         raw_text.value += " " + mult_sinewave[i].toString() + ","; 
- 
+        raw_text.value += userwave[i].toString() + ", "; 
+        
+         //as we need to send two arrays to the chart, we create one just for inumeration for the y axis
+        y_axis[i] = i; 
      }
-     var y_axis = new Array(); 
-     for (let i=0; i<TABLE_SIZE; i++) y_axis[i] = i; 
      
-         build_chart(mult_sinewave,y_axis); 
+     //we ship the arrays to the charts
+     build_chart(userwave,y_axis); 
 }
-
-
 
 function build_chart(x_axis_set, y_axis_ref)
 {
+    // we need to destroy the content of the chart before using it again. Apparently the variable declared in 
+    // functions remains "static" in javascript, so we need to clean them up ! 
+  
+    if(chart) chart.destroy(); 
+    
     var ctx = document.getElementById("myChart");
     
-    new Chart(ctx, {
+   chart = new Chart(ctx, {
   type: "line",
   data: {
     labels: y_axis_ref,
@@ -111,24 +138,3 @@ function build_chart(x_axis_set, y_axis_ref)
     
 }
 
-/*
-new Chart("myChart", {
-  type: "line",
-  data: {
-    labels: xValues,
-    datasets: [{
-      fill: false,
-      lineTension: 0,
-      backgroundColor: "rgba(0,0,255,1.0)", 
-      borderColor: "rgba(255,0,255,0.5)",     
-      data: yValues//yValues
-    }]
-  },
-  options: {
-    legend: {display: false},
-    scales: {
-      yAxes: [{ticks: {min: 6, max:16}}],
-    }
-  }
-});
- */
